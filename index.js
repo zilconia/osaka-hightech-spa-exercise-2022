3795edacf59efb2261dd8c95821da356e1ee1c32
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2022-06-22 11:25:15 +0900

    北海道・東北地方用のチェックボックスを v-for で描画

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const RootComponent = {};
+const RootComponent = {
+  data() {
+    return {
+      estates: [
+        '北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県',
+      ],
+    };
+  },
+};
 
 Vue.createApp(RootComponent).mount('#app');
